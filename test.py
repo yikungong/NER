6d90164d2a569f8bf2fed294e39bfe49c131b92b
yikungong <501344806@qq.com>
2019-03-08 12:11:16 +0800

    test git

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
 from numpy import *
+import operator
 
-for x in range(1, 11):
-    print('{0:2d} {1:3d} {2:4d}'.format(x * x, x, x * x * x))
+def createDataSet():
+    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    print group, labels
+    return group, labels
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1))-dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis = 1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.iteritems(),
+                              key = operator.itemgetter(1), reverse = True)
+    return sortedClassCount[0][0]
